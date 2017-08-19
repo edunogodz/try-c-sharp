f65d48b49f87e58a315283f90b93a08235714d38
edunogodz <edunogodz@gmail.com>
2017-08-19 13:35:49 -0300

    Lesson 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace try_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a message");
+            Console.WriteLine("You said " + Console.ReadLine());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # try-c-sharp
+http://campus.codeschool.com/courses/try-c-sharp
+
+dotnet new console
+dotnet restore
+dotnet run
