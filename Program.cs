2e7af3d06306d4c95a0c66db9854024e516bed30
edunogodz <edunogodz@gmail.com>
2017-08-19 14:17:11 -0300

    Lesson 3

--- Program.cs
+++ Program.cs
@@ -9,8 +9,21 @@ namespace try_c_sharp
             Console.WriteLine("What is the name your band?");
             String name = Console.ReadLine();
             Console.WriteLine("How many people are in your band?");
-            int numberOfMembers = int.Parse(Console.ReadLine());
-            Console.WriteLine(name + " has "+ numberOfMembers + " members");
+            int numberOfMembers = 0;
+            if(!int.TryParse(Console.ReadLine(),out numberOfMembers))
+            {
+                Console.WriteLine("input was not valid");
+                Environment.Exit(0);
+            }
+
+            if (numberOfMembers < 1)
+                Console.WriteLine("You must have at least 1 member");
+            else if (numberOfMembers == 1)
+                Console.WriteLine(name + " is a solo");
+            else if (numberOfMembers == 2)
+                Console.WriteLine(name + " is a duo");
+            else
+                Console.WriteLine(name + " has "+ numberOfMembers + " members");
         }
     }
 }
